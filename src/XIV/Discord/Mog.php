81581311884dde555ece01007df852a9ba2702c7
Josh Freeman <josh@viion.co.uk>
2019-04-05 18:18:44 +0100

    Woop discord

---
+++ src/XIV/Discord/Mog.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace XIV\Discord;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+
+class Mog
+{
+    const ENDPOINT = 'https://mog.xivapi.com/mog/notify';
+
+    private function send(array $json = null)
+    {
+        (new Client())->post(self::ENDPOINT, [
+            RequestOptions::JSON => $json,
+            RequestOptions::QUERY => [
+                'key' => getenv('DISCORD_BOT_USAGE_KEY')
+            ],
+        ]);
+    }
+
+    /**
+     * Post a message via mog
+     */
+    public function sendMessage(int $channel = null, string $content = null, array $embed = null)
+    {
+        $this->send([
+            'channel' => $channel,
+            'content' => $content,
+            'embed' => $embed
+        ]);
+    }
+}
