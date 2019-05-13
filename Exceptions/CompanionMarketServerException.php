7912febc65f88f6364401050bb2f80eb3141800c
Josh Freeman <josh@viion.co.uk>
2019-05-13 12:53:59 +0100

    Update servers error.

--- Exceptions/CompanionMarketServerException.php
+++ Exceptions/CompanionMarketServerException.php
@@ -9,5 +9,5 @@ class CompanionMarketServerException extends HttpException
     use ExceptionTrait;
     
     const CODE    = 400;
-    const MESSAGE = 'Invalid server provided for request.';
+    const MESSAGE = 'Invalid server provided for request. For individual Servers use: servers=1,2,3 for Data-Center use: dc=X';
 }
